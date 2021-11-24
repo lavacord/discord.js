197f0ecd98e571cbc438b2232924753f69e49b89
Papa <bradleyjplatt@yahoo.com>
2021-11-23 20:31:55 -0700

    remove refs to v11

--- src/index.ts
+++ src/index.ts
@@ -12,11 +12,6 @@ export class Manager extends LavacordManager {
             if (this.client.guilds.cache) {
                 const guild = this.client.guilds.cache.get(packet.d.guild_id);
                 if (guild) return guild.shard.send(packet);
-            } else {
-                // @ts-ignore
-                const guild = this.client.guilds.get(packet.d.guild_id);
-                // @ts-ignore
-                if (guild) return typeof this.client.ws.send === "function" ? this.client.ws.send(packet) : guild.shard.send(packet);
             }
         };
 
@@ -25,29 +20,12 @@ export class Manager extends LavacordManager {
             this.shards = client.options.shardCount || 1;
         });
 
-        if (client.guilds.cache && typeof (this.client.ws as any).send === "undefined") {
-            client.ws
-                .on("VOICE_SERVER_UPDATE", this.voiceServerUpdate.bind(this))
-                .on("VOICE_STATE_UPDATE", this.voiceStateUpdate.bind(this))
-                .on("GUILD_CREATE", async data => {
-                    for (const state of data.voice_states) await this.voiceStateUpdate({ ...state, guild_id: data.id });
-                });
-        } else {
-            // @ts-ignore
-            client.on("raw", async (packet: DiscordPacket) => {
-                switch (packet.t) {
-                    case "VOICE_SERVER_UPDATE":
-                        await this.voiceServerUpdate(packet.d);
-                        break;
-                    case "VOICE_STATE_UPDATE":
-                        await this.voiceStateUpdate(packet.d);
-                        break;
-                    case "GUILD_CREATE":
-                        for (const state of packet.d.voice_states) await this.voiceStateUpdate({ ...state, guild_id: packet.d.id });
-                        break;
-                }
+        client.ws
+            .on("VOICE_SERVER_UPDATE", this.voiceServerUpdate.bind(this))
+            .on("VOICE_STATE_UPDATE", this.voiceStateUpdate.bind(this))
+            .on("GUILD_CREATE", async data => {
+                for (const state of data.voice_states) await this.voiceStateUpdate({ ...state, guild_id: data.id });
             });
-        }
     }
 
 }
